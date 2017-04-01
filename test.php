384be7b8d965beb7120effd2e2b7e9871eec72bd
smile_ucas <xiaoxiaotvb@126.com>
2017-04-01 14:15:50 +0800

    first commit

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Demo</title>
+</head>
+
+<body>
+
+ <h1>这是一个测试php的页面:
+   <?php
+     echo "hello world"
+    ?>
+ </h1>
+
+
+</body>
+</html>
